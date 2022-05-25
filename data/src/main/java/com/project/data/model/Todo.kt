76bc8eda5d2091797d88d08c603227d91f245712
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-05-25 18:09:19 +0900

    Entity todoTable 생성

---
+++ data/src/main/java/com/project/data/model/Todo.kt
@@ -0,0 +1,14 @@
+package com.project.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todoTable")
+data class Todo(
+    @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) var id: Long = 0,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "timestamp") val timestamp: String,
+    @ColumnInfo(name = "isChecked") var isChecked: Boolean
+)
