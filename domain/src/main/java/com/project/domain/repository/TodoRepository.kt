8440e77c6a1d7afdf6f1000f06f9501052bff01a
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-05-25 18:20:46 +0900

    interface TodoRepository 구현

--- domain/src/main/java/com/project/domain/repository/TodoRepository.kt
+++ domain/src/main/java/com/project/domain/repository/TodoRepository.kt
@@ -1,4 +1,12 @@
 package com.project.domain.repository
 
+import androidx.lifecycle.LiveData
+import com.project.domain.model.Todo
+
 interface TodoRepository {
+    fun list(): LiveData<MutableList<Todo>>
+    fun getTodo(id: Long): Todo
+    fun insert(todo: Todo)
+    fun update(todo: Todo)
+    fun delete(todo: Todo)
 }
