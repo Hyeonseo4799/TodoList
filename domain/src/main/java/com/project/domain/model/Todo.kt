56bcf5382bbec975b4c07642d28092e80936c1e1
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-05-27 08:56:42 +0900

    Intent 사용하기 위해 Serializable

--- domain/src/main/java/com/project/domain/model/Todo.kt
+++ domain/src/main/java/com/project/domain/model/Todo.kt
@@ -1,9 +1,11 @@
 package com.project.domain.model
 
+import java.io.Serializable
+
 data class Todo(
     var id: Long = 0,
     val title: String,
     val content: String,
     val timestamp: String,
     var isChecked: Boolean
-)
+): Serializable
