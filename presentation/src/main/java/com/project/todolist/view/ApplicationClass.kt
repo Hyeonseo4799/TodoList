efcca6ee5791419a19d269a21f159599892cc2cc
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-05-27 08:53:16 +0900

    startKoin

--- presentation/src/main/java/com/project/todolist/view/ApplicationClass.kt
+++ presentation/src/main/java/com/project/todolist/view/ApplicationClass.kt
@@ -2,11 +2,18 @@ package com.project.todolist.view
 
 import android.app.Application
 import com.project.data.datasource.TodoDataSourceImpl
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-class ApplicationClass: Application() {
+class ApplicationClass : Application() {
     override fun onCreate() {
         super.onCreate()
 
         TodoDataSourceImpl.initialize(this)
+
+        startKoin {
+            androidContext(this@ApplicationClass)
+            modules(updateModule, listModule, insertModule, getTodoModule, todoDataSourceModule, todoRepositoryModule, deleteModule, viewModelModule)
+        }
     }
 }
