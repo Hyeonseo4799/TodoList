8160221a10198845ca2321dcaab7b21616d22d7c
Hyeonseo4799 <s21035@gsm.hs.kr>
2022-05-27 12:21:05 +0900

    return Todo

--- domain/src/main/java/com/project/domain/usecase/GetTodoUseCase.kt
+++ domain/src/main/java/com/project/domain/usecase/GetTodoUseCase.kt
@@ -1,9 +1,8 @@
 package com.project.domain.usecase
 
+import com.project.domain.model.Todo
 import com.project.domain.repository.TodoRepository
 
 class GetTodoUseCase(private val repository: TodoRepository) {
-    fun invoke(id: Long) {
-        repository.getTodo(id)
-    }
+    fun invoke(id: Long): Todo = repository.getTodo(id)
 }
